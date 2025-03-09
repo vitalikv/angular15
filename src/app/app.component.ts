3189b9ee948fb5fe516683e41e3f06563b1e4d46
vit <9455469@mail.ru>
2025-03-09 19:16:48 +0300

    2 add worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<h1>Angular Web Worker</h1>
+    <button (click)="sendMessage()">Send Message</button>
+    <p>{{ message }}</p>`,
 })
-export class AppComponent {
-  title = 'angular15';
+export class AppComponent implements OnInit {
+  private worker: Worker | undefined;
+  message = 'No message yet';
+
+  ngOnInit() {
+    if (typeof Worker !== 'undefined') {
+      this.worker = new Worker(new URL('./worker.worker', import.meta.url));
+      this.worker.onmessage = ({ data }) => {
+        this.message = data;
+      };
+    } else {
+      console.warn('Web Workers are not supported in this environment.');
+    }
+  }
+
+  sendMessage() {
+    if (this.worker) {
+      this.worker.postMessage('Hello from AppComponent');
+    }
+  }
 }
